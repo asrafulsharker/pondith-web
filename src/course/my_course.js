166b58a9b499a564187cf25b7495c021ed2bc472
asrafulnirob1376 <asrafulsharkernirob1376@gmail.com>
2021-01-25 16:21:35 +0600

    add8

--- src/course/my_course.js
+++ src/course/my_course.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Course_nav from './course_nav';
+import progress from '../images/progress.png'
 function course_home() {
     return (
         <div>
@@ -16,9 +17,32 @@ function course_home() {
                     <p className="m-auto"  style={{color:"rgba(128, 118, 118, 1)",padding:"4px 5px",textAlign:"center",fontSize:"20px"}}>Religion</p>
                     
                 </div>
+                <div className="row">
+                    
+                    <div  className="col-md-2  ">
+                        <div className="left-sm-box-course">
+                        <a className="row-h" href="#" style={{textDecoration:"none"}}>
+                        <div className="row left-course-sm-row ">
+                                 <img className="" style={{height:"25px" ,marginLeft:"8%"}} src={progress}/>
+                                  <p style={{marginLeft:"5%"}}>Progress</p>
+                        </div>
+                        </a>
+                        <a className="row-h" href="#" style={{textDecoration:"none"}}>
+                        <div className="row left-course-sm-row ">
+                                <img className="" style={{height:"25px" ,marginLeft:"8%"}} src={progress}/>
+                                 <p style={{marginLeft:"5%"}}>More</p>
+                           </div>
+                        </a>
+                        </div>
+
+                    </div>
+                    
+                    </div>
+
+                </div>
                 
             </div>
-        </div>
+        
     )
 }
 
